48ff4937068bfeafcc558765713c16d1a1263e98
pandyamehul <pandya.mehul@live.com>
2019-09-22 12:33:28 +0200

    minor modification to API

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Mehul Pandya", "Test" };
+            return new string[] { "Dot net", "Enabled CI/CD" };
         }
 
         // GET api/values/5
